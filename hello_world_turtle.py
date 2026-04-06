15e517696fb60f8f54cc5bedb849e78b9a1ed474
sviatoslav-zapolskyi <sviatoslav.zapolskyi@gmail.com>
2026-04-06 17:15:41 +0000

    update hello_world_turtle.py

--- hello_world_turtle.py
+++ hello_world_turtle.py
@@ -1,10 +1,10 @@
 import turtle
 
-ekran = turtle.Screen()
-ekran.title("Hello World - Turtle")
+screen = turtle.Screen()
+screen.setup(width=1.0, height=1.0)
+screen.title("Hello World - Turtle")
 
 pen = turtle.Turtle()
 pen.forward(100)
 
 turtle.done()
-
